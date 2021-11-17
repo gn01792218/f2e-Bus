3f9e059f2046bce6b6bbf1555c2cbeed1ae9716a
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2021-11-17 13:59:08 +0800

    API:上架API

--- src/api.ts
+++ src/api.ts
@@ -8,14 +8,31 @@ const hotelProperty = "$select=ID%2CName%2CDescription%2CGrade%2CAddress%2CPhone
 const activityProperty = "$select=ID%2CName%2CDescription%2CParticpation%2CLocation%2CAddress%2CPhone%2COrganizer%2CStartTime%2CEndTime%2CWebsiteUrl%2CPicture%2CPosition%2CClass1%2CClass2%2CCharge%2CRemarks&"
 
 
+//公車動態API
+//取得指定[縣市]的公車動態定時資料(A1)[逐筆更新]
+export const getReallTimeBusByCity = (City:City) => {
+    return req('get',`/v2/Bus/RealTimeByFrequency/Streaming/City/${City}`)
+}
+//取得指定[縣市],[路線名稱]的公車動態定時資料(A1)[逐筆更新]
+export const getReallTimeBusByCityAndRoute = (City:City,RouteName:number) => {
+    return req('get',`GET /v2/Bus/RealTimeByFrequency/Streaming/City/${City}/${RouteName}`)
+}
+//取得指定[縣市]的公車動態定點資料(A2)[逐筆更新]
+export const getReallTimeNearStopByCity = (City:City) => {
+    return req('get',`/v2/Bus/RealTimeNearStop/Streaming/City/${City}`)
+}
+//取得指定[縣市],[路線名稱]的公車動態定點資料(A2)[逐筆更新]
+export const getReallTimeNearStopByCityAndRoute = (City:City,RouteName:number) => {
+    return req('get',`/v2/Bus/RealTimeNearStop/Streaming/City/${City}/${RouteName}`)
+}
 //預估到站API
 //取得指定縣市的公車預估到站資料
-export const getEstimatedTimeByCity = (City?:City) => {
+export const getEstimatedTimeOfArrivalByCity = (City?:City) => {
     return req('get',`/v2/Bus/EstimatedTimeOfArrival/Streaming/City/${City}`)
 //   return req('get',`/v2/Tourism/ScenicSpot?${sceneProperty}$top=${top}&$format=JSON`)
 }
 //取得指定[縣市],[路線名稱]的公車預估到站資料(N1)[逐筆更新]
-export const getEstimatedTimeByCityAndRouteName = (City:City,RouteName:number) => {
+export const getEstimatedTimeOfArrivalByCityAndRouteName = (City:City,RouteName:number) => {
     return req('get',`/v2/Bus/EstimatedTimeOfArrival/Streaming/City/${City}/${RouteName}`)
 //   return req('get',`/v2/Tourism/ScenicSpot?${sceneProperty}$top=${top}&$format=JSON`)
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    currentCity:"", //當前選擇的縣市
   },
   mutations: {
   },
